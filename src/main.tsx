6ea904eaa63c5b659bcd499a96c9e6cef618201a
MastaDEV <tankslimcom@gmail.com>
2023-02-03 00:07:44 +0100

    stepa1

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+// import App from './App'
+import './index.css';
+
+import Router from 'routes/Router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+   <React.StrictMode>
+      {/* <App /> */}
+      <RouterProvider router={Router} />
+   </React.StrictMode>,
+);
